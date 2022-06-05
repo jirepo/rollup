b315d845d906fefea3c217c2873c6693aa684eb2
latteonterrace <latteonterrace@gmail.com>
2022-06-05 10:26:34 +0900

    시작

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+// rollup.config.js
+
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: './src/app.ts',
+  output: {
+    file: './dist/bundle.js',
+    format: 'iife'
+  },
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json'
+    })
+  ]
+}
